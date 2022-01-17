dfbf2f82baa1955d8f4d4dba5070bee5293c0060
alpaunovski <75141862+alpaunovski@users.noreply.github.com>
2022-01-17 14:59:18 +0200

    auth

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 //jshint esversion:6
 
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-
+const Schema = mongoose.Schema;
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 const app = express();
 
 app.use(express.static("public"));
@@ -14,12 +17,14 @@ app.use(bodyParser.urlencoded({
 }));
 
 
+
 mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true });
 
-const userSchema = {
+
+const userSchema = new Schema( {
     email: String,
     password: String
-};
+});
 
 const User = new mongoose.model("User", userSchema);
 
@@ -35,25 +40,32 @@ app.get("/register", function(req, res) {
     res.render("register");
 });
 
-app.post("/register", function(req, res) {
-    const newUser = new User({
-        email: req.body.username,
-        password: req.body.password
-    });
 
-    newUser.save(function(err) {
-        if (err) {
-            console.log(err);
-        } else {
-            res.render("secrets");
-        }
-    })
+
+app.post("/register", function(req, res) {
+    const myPlaintextPassword = req.body.password;
+
+    bcrypt.hash(myPlaintextPassword, saltRounds, function(err, hash) {
+        // Store hash in your password DB.
+    
+        const newUser = new User({
+            email: req.body.username,
+            password: hash
+        });
+    
+        newUser.save(function(err) {
+            if (err) {
+                console.log(err);
+            } else {
+                res.render("secrets");
+            }
+        })
+    });
 });
 
 app.post("/login", function(req, res) {
-
+    const myPlaintextPassword = req.body.password;
     const username = req.body.username;
-    const password = req.body.password;
 
     User.findOne({ email: username },
         function(err, foundUser) {
@@ -61,9 +73,14 @@ app.post("/login", function(req, res) {
                 console.log(err);
             } else {
                 if (foundUser) {
-                    if (foundUser.password === password) {
-                        res.render("secrets");
-                    }
+                    const hash = foundUser.password;
+
+                    bcrypt.compare(myPlaintextPassword, hash, function(err, result) {
+                        // result == true
+                        if (result) {
+                            res.render("secrets");
+                        }
+                    });
                 }
             }
         }
